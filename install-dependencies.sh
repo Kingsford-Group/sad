1bb9e9c60f34cb447fc914e9686ab13f38cb8753
Cong Ma <congm1@andrew.cmu.edu>
2020-02-06 13:48:01 -0500

    update clp and boost installation

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 # install the following libraries using bash in linux
 # boost, eigen, gsl, jellyfish, htslib, spline
@@ -17,9 +18,9 @@ cd ${Dir}/external
 wget https://dl.bintray.com/boostorg/release/1.69.0/source/boost_1_69_0.tar.gz
 tar -xzvf boost_1_69_0.tar.gz
 cd ${Dir}/external/boost_1_69_0
-mkdir Installation
-./bootstrap.sh --prefix=${Dir}/external/boost_1_69_0/Installation
-./b2 install
+mkdir -p Installation
+./bootstrap.sh --with-libraries=iostreams --prefix=${Dir}/external/boost_1_69_0/Installation
+./b2 install 
 
 
 # install eigen
@@ -93,10 +94,11 @@ if $(pkg-config --exists clp); then
 else
 	echo "Install clp..."
 	cd ${Dir}/external
-	svn co https://projects.coin-or.org/svn/Clp/stable/1.17 coin-Clp
-	cd coin-Clp/
+	wget https://www.coin-or.org/download/source/Clp/Clp-1.17.5.tgz
+	tar -xzvf Clp-1.17.5.tgz
+	cd Clp-1.17.5/
 	mkdir Installation
-	./configure -C --prefix=${Dir}/external/coin-Clp/Installation
+	./configure -C --prefix=${Dir}/external/Clp-1.17.5/Installation
 	make
 	make install
 fi
@@ -113,5 +115,5 @@ wget https://kluge.in-chemnitz.de/opensource/spline/spline.h
 mkdir -p ${Dir}/external/pkgconfig
 cp -n $(find ${Dir}/external/ -name *.pc) ${Dir}/external/pkgconfig/
 
-echo "To configure with CLP, run: configure PKG_CONFIG_PATH=${PKG_CONFIG_PATH}"
-echo "To configure with gurobi, run: configure PKG_CONFIG_PATH=${PKG_CONFIG_PATH} --with-gurobi=<path to gurobi linux64 folder>"
+echo "To configure with CLP, run: configure PKG_CONFIG_PATH=${PKG_CONFIG_PATH} BOOST_ROOT=${Dir}/external/boost_1_69_0/Installation"
+echo "To configure with gurobi, run: configure PKG_CONFIG_PATH=${PKG_CONFIG_PATH} --with-gurobi=<path to gurobi linux64 folder> BOOST_ROOT=${Dir}/external/boost_1_69_0/Installation"
